4afb1fed87ea568f877e8439ac323739b0ebdeb8
KolemanPa <46792228+KolemanPa@users.noreply.github.com>
2019-04-03 13:43:49 -0500

    some stuff

--- Calendar/Calandar.js
+++ Calendar/Calandar.js
@@ -1,6 +1,9 @@
+var moment = require('moment');
+moment().format();
+
+
 const startWeek = moment().startOf('month').week();
 const endWeek = moment().endOf('month').week();
-
 
 let calendar = []
 for(var week = startWeek; week<endWeek;week++){
@@ -8,4 +11,19 @@ for(var week = startWeek; week<endWeek;week++){
     week:week,
     days:Array(7).fill(0).map((n, i) => moment().week(week).startOf('week').clone().add(n + i, 'day'))
   })
+}
+
+console.log(calendar);
+
+
+
+
+var calen = [];
+function printCalander(){
+for(var week = startWeek; week<endWeek;week++){
+    calen.push({
+      
+  })
+}
+
 }
