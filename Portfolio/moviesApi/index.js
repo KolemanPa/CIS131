f3dad93610c7533c8dc44ef5e8611d84b46502f7
KolemanPa <kp0885055@otc.edu>
2019-05-01 13:12:56 -0500

    OH NO

--- Portfolio/moviesApi/index.js
+++ Portfolio/moviesApi/index.js
@@ -15,14 +15,46 @@ $(document).ready(function () {
             console.log(httpRequestMovies.readyState);
             var moviesObj = JSON.parse(httpRequestMovies.responseText);
             if (httpRequestMovies.readyState == 4 && httpRequestMovies.status == 200) {
-                document.getElementById('app').innerHTML = `<card source="http://image.tmdb.org/t/p/w185/${moviesObj.results[i].poster_path}" text="${moviesObj.results[i].overview}"></card>`
+                document.getElementById('app').innerHTML = `<card source="http://image.tmdb.org/t/p/w185/${moviesObj.results[i].poster_path}" text="${moviesObj.results[i].overview}" id="${i}" onload="vue()"></card>`
             }
         }
     });
+
+    function vue() {
+        Vue.component('butt', {
+            props: ['buttname', "buttage"],
+            template: `<button @click='count++'> {{buttname}} was clicked {{count}} times.</button>`,
+            data: function () {
+                return {
+                    count: 0
+                }
+            }
+        });
+
+
+        Vue.component('card', {
+            props: ['source', 'text'],
+            template: ` <div class="movieDesc" style="width: 18rem;">
+                <img :src="source" class="card-img-top" alt="Picture">
+                <div class="card-body">
+                <h5 class="card-title">movieDesc title</h5>
+                <p class="card-text">{{text}}</p>
+                <butt class="btn btn-primary">Go somewhere</butt>
+                </div>
+                </div>`
+        });
+
+        var app = new Vue({
+            el: "#app",
+            data: {
+                message: "Again"
+            }
+        })
+
+
+
+        function clearPage() {
+            document.getElementById('beginForm').innerHTML = "";
+        }
+    }
 });
-
-
-
-function clearPage(){
-    document.getElementById('beginForm').innerHTML = "";
-}
